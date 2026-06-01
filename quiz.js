4c45a2b142e57a2451c0ecc6da526ca81a1acf39
vesnny-hub <dantheprogrammer11@gmail.com>
2026-06-01 22:42:42 +0100

    Add files via upload

---
+++ quiz.js
@@ -0,0 +1,151 @@
+const questions = [
+    {question: "What is the brightest color according to scientific research",
+        answers: [{ text:'Blue', correct: false},
+                 {text: 'White', correct: true},
+                 {text: 'yellow', correct: false},
+                 {text: 'red', correct: false}
+                ]
+               
+},
+{
+    question: "Which is the smallest country in the world",
+    answers: [
+        {text: "USA", correct: false},
+        {text: "Egypt", correct: false},
+        {text: "Singapore", correct: false},
+        {text: "Vatican city", correct: true}
+    ]
+}, {
+    question: "Which is the largest country in the world",
+    answers: [
+        {text: "USA", correct: false},
+        {text: "Russia", correct: false},
+        {text: "Singapore", correct: false},
+        {text: "China", correct: false}
+    ]
+}
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+const submitButton = document.getElementById('submitButton');
+
+
+decision = nextButton
+
+let currentQuestionIndex = 0;
+let score = 0;
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    
+    showQuestion();
+    watchReset();
+    
+    
+}
+
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    submitButton.style.display = "none"; 
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    decision.style.display = "block";
+}
+
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = 'Play Again!' + currentQuestionIndex;
+    decision.style.display = "block"
+    watchStop();
+}
+
+
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex == 4 ){
+        decision = submitButton;
+    }else{
+        decision = nextButton;
+    }
+    nextButton.innerHTML = "Next";
+    if (currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+decision.addEventListener("click", ()=> {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        startQuiz();
+    }
+});
+
+
+startQuiz();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
